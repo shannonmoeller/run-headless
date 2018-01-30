51c4c217ab3bff313a0fd8f10b28c0f3b5dc2fd3
Shannon Moeller <me@shannonmoeller.com>
2018-01-29 22:42:36 -0700

    Remove close helper

--- close.js
+++
@@ -1,9 +0,0 @@
-// Convenience method to close window, if needed.
-// Useful for indicating test completion.
-function close() {
-	if (typeof window !== 'undefined') {
-		window.__close__();
-	}
-}
-
-module.exports = close;
